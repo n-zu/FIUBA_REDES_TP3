38cce92fbee277bd97fb0bb26476d067dd994310
Nicolas Zulaica <nzulaica@fi.uba.ar>
2022-06-27 19:55:01 -0700

    script pingall

--- scripts/topo.sh
+++ scripts/topo.sh
@@ -1,1 +1,10 @@
-sudo python linear_topo.py
+#!/bin/bash
+
+# if $1 is not empty
+
+if [ -z "$1" ]; then
+  sudo python topology/linear_topo.py
+  exit 1
+fi
+
+sudo python topology/$1.py
---
+++ topology/pingall.py
@@ -0,0 +1,24 @@
+from linear_topo import linear_topo
+from mininet.node import RemoteController
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+
+def main():
+    setLogLevel("info")
+
+    n_switches = int(input("Enter number of switches: "))
+
+    net = linear_topo(n_switches)
+
+    info("*** Running Script\n")
+    net.pingAll()
+
+    info("*** Running CLI\n")
+    CLI(net)
+
+    info("*** Stopping network")
+    net.stop()
+
+
+if __name__ == "__main__":
+    main()
