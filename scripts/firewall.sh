9ff9e8d6b2e88304d0be22fa98fdfcbb8c832402
Felipe de Luca <71850278+fdelu@users.noreply.github.com>
2022-06-30 16:24:59 -0300

    Update firewall.sh

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -1,1 +1,1 @@
-poetry run ./pox/pox.py log.level --DEBUG openflow.of_01 forwarding.l2_learning firewall
+poetry run ./pox/pox.py log.level --DEBUG openflow.of_01 forwarding.l2_learning log.color firewall
